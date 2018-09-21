ae56868c4c8ceaa824c82904aa68c7c19c50872e
Reihms <Bryon@Reihms.com>
2018-09-20 21:29:19 -0500

    Added the S3 website uploader to the webotron script

--- 01-webotron/webotron/webotron.py
+++ 01-webotron/webotron/webotron.py
@@ -1,5 +1,6 @@
 import boto3
 import click
+from botocore.exceptions import ClientError
 
 session = boto3.Session(profile_name='pythonAutomation')
 s3 = session.resource('s3')
@@ -21,6 +22,55 @@ def list_bucket_objects(bucket):
     "List objects in an S3 bucket, specified by user input provided when executing script"
     for object in s3.Bucket(bucket).objects.all():
         print(object)
+
+@cli.command('setup-bucket')
+@click.argument('bucket')
+def setup_bucket(bucket):
+    "Create and Configure S3 Bucket specified by user"
+    s3_bucket = None
+    try:
+        s3_bucket = s3.create_bucket(
+        Bucket=bucket,
+        CreateBucketConfiguration={'LocationConstraint': session.region_name}
+    )
+    except ClientError as error:
+        if error.response['Error']['Code'] == 'BucketAlreadyOwnedByYou':
+            s3_bucket = s3.Bucket(bucket)
+        else:
+            raise error
+
+    policy = """
+    {
+        "Version":"2012-10-17",
+        "Statement":[{
+            "Sid":"PublicReadGetObject",
+            "Effect":"Allow",
+            "Principal": "*",
+            "Action":["s3:GetObject"],
+            "Resource":["arn:aws:s3:::%s/*"
+            ]
+        }
+    ]
+  }
+    """ % s3_bucket.name
+    policy = policy.strip()
+
+    pol = s3_bucket.Policy()
+    pol.put(Policy=policy)
+
+    website = s3_bucket.Website()
+    website.put(WebsiteConfiguration={
+        'ErrorDocument': {
+            'Key': 'error.html'
+        },
+        'IndexDocument': {
+            'Suffix': 'index.html'
+        }
+    })
+
+    return
+
+
 
 if __name__ == '__main__':
     cli()
